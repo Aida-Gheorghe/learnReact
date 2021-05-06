883b400f06756edc5067ec251472dc37fa42395c
Aida-Gheorghe <aida.andreea.gheorghe@digitalexplorer.ro>
2021-05-07 01:07:45 +0300

    dynamic values y/n

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css"
 
 function App() {
+  // dynamic value accepted > sting, number, array
+  //dynamic value that are not accepted > object, boolean
   const myTitle = "This is my first blog:)"
   return (
     <div className="App">
