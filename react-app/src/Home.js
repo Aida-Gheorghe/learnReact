b5c5dd056f0044bf5bd0b0bf0b7a06e78bf6dd18
Aida-Gheorghe <aida.andreea.gheorghe@digitalexplorer.ro>
2021-05-07 16:57:36 +0300

    reactive values

--- react-app/src/Home.js
+++ react-app/src/Home.js
@@ -1,33 +1,21 @@
+import { useState } from "react"
+
 const Home = () => {
-  // learn events
-
-  const myName = "Ana"
-
-  const selfInvoked = () => {
-    console.log("selfInvoked function")
-  }
+  //reactive values
+  const [name, setName] = useState("Ana")
+  const [age, setAge] = useState(25)
 
   const handleClick = () => {
-    console.log("invoke on click This is a test")
+    setName("Miky")
+    setAge(30)
   }
 
-  const anonymousFunc = (name, e) => {
-    console.log(`My name is ${name}.`)
-    console.log(e.target)
-  }
   return (
     <div className="home">
-      <h2>Homepage</h2>
-
-      {/*self invoked!*/}
-      <button onClick={selfInvoked()}>func self invoked</button>
-
-      {/*refrence to a function*/}
-      <button onClick={handleClick}>Click here</button>
-
-      {/*give a parameter to a func*/}
-      <button onClick={(e) => anonymousFunc(myName, e)}>anonymous btn</button>
-      {/*you can access e in anonymousFunc only if you pass as a param */}
+      <p>
+        {name} is {age} years old
+      </p>
+      <button onClick={handleClick}>Click me!</button>
     </div>
   )
 }
